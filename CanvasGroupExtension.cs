685ebd619885ce3f6f75cf1831936c2251bca03f
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-02 18:40:26 +0700

    1.1.0

---
+++ CanvasGroupExtension.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UnityExtensions
+{
+    public static class CanvasGroupExtension
+    {
+        public static void SetAlpha(this CanvasGroup canvasGroup, double value)
+        {
+            canvasGroup.alpha = (float)value;
+        }
+    }
+}
---
+++ UnityGraphicRaycaster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityExtensions
+{
+    public class UnityGraphicRaycaster : MonoBehaviour
+    {
+        [SerializeField] private GraphicRaycaster raycaster;
+        [SerializeField] private ScriptableObjectGraphicRaycaster sRaycaster;
+
+        private void Awake()
+        {
+            sRaycaster.Impl = raycaster;
+        }
+    }
+}
