0ae40bda61d5b873f12a2fe31abb0a491fe76425
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-08 11:52:36 +0700

    2.1.0

---
+++ ObjectReference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace UnityExtensions
+{
+    [Serializable]
+    public class ObjectReference<T> where T : class
+    {
+        [SerializeField] private Object target;
+        public T Value => target as T;
+    }
+}
