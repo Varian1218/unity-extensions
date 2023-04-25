41a605bf37b9a3fc8d8be597c85ec711977de965
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-25 21:36:52 +0700

    3.3.0

--- PopupPropertyAttribute.cs
+++ PopupPropertyAttribute.cs
@@ -1,10 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
 
 namespace UnityBoosts
 {
     public class PopupPropertyAttribute : PropertyAttribute
     {
         public string[] Options { get; }
+
+        public PopupPropertyAttribute(Type type)
+        {
+            Options = type.GetFields(BindingFlags.Public | BindingFlags.Static).Select(it => (string)it.GetValue(null))
+                .ToArray();
+        }
 
         public PopupPropertyAttribute(params string[] options)
         {
