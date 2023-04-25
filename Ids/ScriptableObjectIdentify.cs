400876cf89a2bc64cfcf16accfb1af80f268a775
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-25 20:46:33 +0700

    3.1.0

---
+++ Ids/ScriptableObjectIdentify.cs
@@ -0,0 +1,16 @@
+﻿using CSharpBoosts;
+
+namespace UnityBoosts.Ids
+{
+    public class ScriptableObjectIdentify : IId
+    {
+        private IId _impl;
+
+        public IId Impl
+        {
+            set => _impl = value;
+        }
+
+        public int Value => _impl.Value;
+    }
+}
